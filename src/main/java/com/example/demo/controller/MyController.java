93d1cefa282add2d672b214a366a9075ad179898
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2018-09-13 10:01:59 -0400

    Return json

--- src/main/java/com/example/demo/controller/MyController.java
+++ src/main/java/com/example/demo/controller/MyController.java
@@ -1,7 +1,12 @@
 package com.example.demo.controller;
 
 import com.example.demo.entity.A;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,9 +14,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 @Controller
 public class MyController {
 
-    @PostMapping(value = "/demo", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public String demo(@RequestBody A data) {
-        // error
-        return "";
+    @PostMapping(value = "/demo", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> demo(@RequestBody A data) throws JsonProcessingException {
+        return new ResponseEntity<>(new ObjectMapper().writeValueAsString(data), new HttpHeaders(), HttpStatus.OK);
     }
 }
